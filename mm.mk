2c66798cda625a56dce7f3844cc4e9d6fc22c03f
SunRain <41245110@qq.com>
2014-02-22 18:20:29 +0800

    change to magicmod

--- mm.mk
+++ mm.mk
@@ -1,5 +1,5 @@
-# Inherit some common KM stuff. 
-$(call inherit-product, vendor/kylin/config/common_full_phone.mk)
+# Inherit some common MM stuff. 
+$(call inherit-product, vendor/mm/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 720
@@ -7,7 +7,7 @@ TARGET_SCREEN_HEIGHT := 1280
 
 # Release name
 PRODUCT_RELEASE_NAME := MB886
-PRODUCT_NAME := km_mb886
+PRODUCT_NAME := mm_mb886
 
 $(call inherit-product, device/motorola/mb886/full_mb886.mk)
 
